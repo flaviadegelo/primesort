a52fbba4102f891f1b3307611e62ee8d2372824d
Flavia Degelo Camilo <flaviadegelocamilo@MacBook-Pro-de-Flavia.local>
2017-11-26 16:18:52 -0200

    codigo funcionando

--- src/main.c
+++ src/main.c
@@ -1,17 +1,38 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
+#include <stdlib.h>
+
+int primo(int numero){
+	if(numero == 1){
+		return 0;
+	} else if (numero == 2){
+		return 1;	
+	} else {
+		for(int div = 2 ; div <= numero/2 ; div += 1 ){
+			if(numero % div == 0){
+				return 0;
+			}			
+		}
+		return 1;	
+	}
+}
 
 int main() {
+	
+	int numero;
+	FILE *pipe_fp;
+	
+	if((pipe_fp = popen("sort -n -r", "w")) == NULL){
+		perror("popen");
+		exit(1);
+	}
 
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+	while(scanf("%d\n", &numero) != -1){
+		if(!primo(numero)){
+			fprintf(pipe_fp, "%d\n", numero);
+		}
+	}
+	   
+	pclose(pipe_fp);
+	return(0);
 }
+
